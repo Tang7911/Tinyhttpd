50869667472fcd9b328d35f2377c8588ca83c4a9
big <3248195330@qq.com>
2026-07-01 22:55:50 +0800

    实现请求处理和文件服务功能，增加GET和POST方法支持

--- httpd.c
+++ httpd.c
@@ -5,6 +5,99 @@
 #include<sys/types.h>
 #include<unistd.h>
 #include<netinet/in.h>
+#include<ctype.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+
+#define ISspace(x) isspace((int)(x))
+
+void accept_request(int client)
+{
+    int n = 0;
+    char buf[254];
+    char url[254];
+    char method[254];
+    char path[254];
+    int cgi = 0;
+    int i = 0;
+    int j = 0;
+    struct stat st;
+    char *query_string = NULL;
+
+    n = get_line(client, buf, sizeof(buf));
+    while(!ISspace(buf[i]) && i < sizeof(method)-1)
+    {
+        method[i] = buf[i];
+        i++;
+    }
+    method[i] = '\0';
+
+    if(!strcasecmp(method, 'POST'))
+    {
+        cgi = 1;
+    }
+
+    j = i;
+    i = 0;
+
+    while(ISspace(buf[j]) && (j < n))
+    {
+        j++;
+    }
+    while(!ISspace(buf[j]) && (j < n))
+    {
+        url[i] = buf[j];
+        i++;
+        j++;
+    }
+    url[i] = '\0';
+
+    if(!strcasecmp(method, 'GET'))
+    {
+        query_string = url;
+        while((*query_string != '?') && (*query_string != '\0'))
+        {
+            query_string++;
+        }
+        if(*query_string == '?')
+        {
+            cgi = 1;
+            *query_string = '\0';
+            query_string++;
+        }
+    }
+    sprintf(path, "htdocs%s", url);
+    if(path[strlen(path)-1] == "/")
+    {
+        strcat(path, '/index.html');
+    }
+
+    if(stat(path, &st) == -1)
+    {
+        not_found();
+    }
+    else
+    {
+        if((st.st_mode & S_IFMT) == S_IFDIR)
+        {
+            strcat(path, "/index.html");
+        }
+        if((st.st_mode & S_IXUSR)
+         || (st.st_mode & S_IXGRP)
+         || (st.st_mode & S_IXOTH))
+        {
+            cgi = 1;
+        }
+        if(!cgi)
+        {
+            serve_file();
+        }
+        else
+        {
+            execute_cgi();
+        }
+    }
+}
 
 int startup(short *port)
 {
@@ -42,6 +135,56 @@ int startup(short *port)
     }
     printf("listen...\n");
     return httpd;
+}
+
+int get_line(int client, char *buf, int size)
+{
+    char c = '\0';
+    int i = 0;
+    int n = 1;
+    while((i < (size-1)) && (c != '\n'))
+    {
+        n = recv(client, &c, 1, 0);
+        if(n > 0)
+        {
+            if(c == '\r')
+            {
+                n = recv(client, &c, 1, MSG_PEEK);
+                if( (n > 0) && (c == '\n'))
+                {
+                    recv(client, &c, 1, 0);
+                }
+                else
+                {
+                    c = '\n';
+                }
+            }
+            buf[i] = c;
+            i++;
+        }
+        else
+        {
+            c = '\n';
+        }
+    }
+    buf[i] = '\0';
+
+    return i;
+}
+
+void not_found()
+{
+
+}
+
+void serve_file()
+{
+
+}
+
+void execute_cgi()
+{
+
 }
 
 int main()
